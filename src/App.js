67d91c95987e73ff0476b0a77c6ebe2f1f4a82e7
Hervey John Lamothe <herveylamothe@gmail.com>
2019-02-20 16:40:38 -0500

    started routing repoistory

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+/*
+  react-router => The core foundational library that the others ones use
+  react-router-dom => The web based library to do dom manipulation. Web Apps
+  react-router-native => The mobile based library for react native apps. Mobile Apps
+*/
+
+/*
+  localhost:3000/
+  localhost:3000/aboutus
+*/
+
+const Home = (props) => {
+  return (
+    <>
+      <h1>Welcome to the home page</h1>
+      <Link to='/aboutus'>About Us</Link>
+      <a href='/aboutus'>About Us</a>
+    </>
+  )
+}
+
+const Aboutus = (props) => {
+  return (
+    <>
+      <h1>About us page</h1>
+      <Link to='/'>Home Page</Link>
+      <a href='/'>Home Page</a>
+    </>
+  )
+}
+
+/*
+
+  userTypedPath : '/aboutus'
+
+  Without Exact:
+    userTypedPath.contains(path)
+  With Exact:
+    userTypedPath === path 
+*/
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <>
+          <Route path='/aboutus' component={Aboutus} />
+          <Route path='/' exact component={Home} />
+        </>
+      </BrowserRouter>
     );
   }
 }
